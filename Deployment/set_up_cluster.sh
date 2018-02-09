1f662f23dd9e7847b8fc1e0b6c514a752dd909f6
Amar Mani Aryal <aryalamar@gmail.com>
2018-02-09 10:52:56 -0500

    Update set_up_cluster.sh

--- Deployment/set_up_cluster.sh
+++ Deployment/set_up_cluster.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+#Install Pegasus https://github.com/InsightDataScience/pegasus
+#Set up Spark <masters.yml and workers.yml contain the information about Spark Cluster>
+peg up master.yml
+peg up woarkers.yml
 
+#Set Up Kafka
+peg up kafka.yml
+
+#Set Up Cassandra
+peg up cassandra.yml
