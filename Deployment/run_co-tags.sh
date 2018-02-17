9d8cb16e2f8a6174931573a197f7d078f1c4615c
Amar Mani Aryal <aryalamar@gmail.com>
2018-02-16 20:44:43 -0500

    Update run_co-tags.sh

--- Deployment/run_co-tags.sh
+++ Deployment/run_co-tags.sh
@@ -3,6 +3,6 @@
 #Run Kafka producer
 peg sshcmd-node kafka-cluster 1 "python /home/ubuntu/producer.py &" 
 # Run spark-streaming script 
-peg sshcmd-node spark-cluster 1 "/usr/local/spark/bin/spark-submit --master spark://ip-10-0-0-13.ec2.internal:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 spark_streaming.py &"
+peg sshcmd-node spark-cluster 1 "/usr/local/spark/bin/spark-submit --master spark://IP.internal:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 spark_streaming.py &"
 # Run Flask 
 peg sshcmd-node flask-cluster 1 "python -E /usr/home/Flask/tornadoapp.py &"
